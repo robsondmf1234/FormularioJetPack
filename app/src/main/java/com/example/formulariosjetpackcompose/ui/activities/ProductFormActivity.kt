470a82fe968171793d4247f39a6da664c813cb25
robsondmf <robsondmf1234@gmail.com>
2025-02-11 16:45:51 -0300

    feat - adicionando elementos a tela de formulario.

--- app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
@@ -3,13 +3,20 @@ package com.example.formulariosjetpackcompose.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.formulariosjetpackcompose.ui.theme.AluveryLazyLayoutTheme
 
-//TODO("https://cursos.alura.com.br/course/jetpack-compose-formulario-gerenciamento-estado/task/116677")
+//TODO("httphttps://cursos.alura.com.br/course/jetpack-compose-formulario-gerenciamento-estado/task/116678")
 class ProductFormActivity : ComponentActivity() {
     // onCreate é chamado quando a atividade é criada pela primeira vez
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,19 +25,55 @@ class ProductFormActivity : ComponentActivity() {
         setContent {
             AluveryLazyLayoutTheme {
                 Surface {
-                    Text(text = "Formulário de Produto")
+                    ProductFormScreen()
                 }
             }
         }
     }
 }
 
-@Preview(showSystemUi = true)
 @Composable
-fun ProductFormPreview() {
-    AluveryLazyLayoutTheme {
-        Surface {
-            Text(text = "Formulário de Produto")
+fun ProductFormScreen() {
+    Column {
+        Text(text = "Criando o produto.")
+        var url by remember {
+            mutableStateOf("")
+        }
+        TextField(value = url, onValueChange = {
+            url = it
+        })
+        var name by remember {
+            mutableStateOf("")
+        }
+        TextField(value = name, onValueChange = {
+            name = it
+        })
+
+        var price by remember {
+            mutableStateOf("")
+        }
+        TextField(value = price, onValueChange = {
+            price = it
+        })
+        var description by remember {
+            mutableStateOf("")
+        }
+        TextField(value = description, onValueChange = {
+            description = it
+        })
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Salvar")
         }
     }
 }
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ProductFormScreenPreview() {
+    AluveryLazyLayoutTheme {
+        Surface {
+            ProductFormScreen()
+        }
+    }
+}
+
