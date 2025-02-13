bd3d9264c44beebe092f80af4e9fe3bf5099e0d6
robsondmf <robsondmf1234@gmail.com>
2025-02-13 10:50:02 -0300

    feat - Ajuste no layout de Formulario.

--- app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
@@ -3,7 +3,13 @@ package com.example.formulariosjetpackcompose.ui.activities
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -13,10 +19,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.formulariosjetpackcompose.ui.theme.AluveryLazyLayoutTheme
 
-//TODO("httphttps://cursos.alura.com.br/course/jetpack-compose-formulario-gerenciamento-estado/task/116678")
+//TODO("https://cursos.alura.com.br/course/jetpack-compose-formulario-gerenciamento-estado/task/116679")
 class ProductFormActivity : ComponentActivity() {
     // onCreate é chamado quando a atividade é criada pela primeira vez
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,34 +43,73 @@ class ProductFormActivity : ComponentActivity() {
 
 @Composable
 fun ProductFormScreen() {
-    Column {
-        Text(text = "Criando o produto.")
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            fontSize = 28.sp,
+            text = "Criando o produto.",
+            modifier = Modifier.fillMaxWidth(),
+        )
         var url by remember {
             mutableStateOf("")
         }
-        TextField(value = url, onValueChange = {
-            url = it
-        })
+        TextField(
+            value = url,
+            onValueChange = {
+                url = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text("Url da imagem")
+            },
+        )
         var name by remember {
             mutableStateOf("")
         }
-        TextField(value = name, onValueChange = {
-            name = it
-        })
+        TextField(
+            value = name,
+            onValueChange = {
+                name = it
+            },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text("Name")
+            },
+        )
 
         var price by remember {
             mutableStateOf("")
         }
-        TextField(value = price, onValueChange = {
-            price = it
-        })
+        TextField(
+            value = price,
+            onValueChange = {
+                price = it
+            },
+            Modifier.fillMaxWidth(),
+            label = {
+                Text("Price")
+            },
+        )
         var description by remember {
             mutableStateOf("")
         }
-        TextField(value = description, onValueChange = {
-            description = it
-        })
-        Button(onClick = { /*TODO*/ }) {
+        TextField(
+            value = description,
+            onValueChange = {
+                description = it
+            },
+            Modifier
+                .fillMaxWidth()
+                .heightIn(100.dp),
+            label = {
+                Text("Description")
+            },
+        )
+        Button(onClick = { /*TODO*/ }, Modifier.fillMaxWidth()) {
             Text(text = "Salvar")
         }
     }
