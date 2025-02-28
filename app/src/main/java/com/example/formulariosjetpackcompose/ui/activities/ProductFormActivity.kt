dae4e7bd771fd2095cf024fd146701f66006f7f2
robsondmf <robsondmf1234@gmail.com>
2025-02-28 16:01:08 -0300

    feat - Ajuste no manifest  para o scroll funcionar quando o teclado estiver ativo ou quando for necessario os elementos estiverem ocupando a tela toda.Adicionado asyncimage para carregar imagem no coil.

--- app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
+++ app/src/main/java/com/example/formulariosjetpackcompose/ui/activities/ProductFormActivity.kt
@@ -6,10 +6,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,9 +24,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import com.example.formulariosjetpackcompose.R
 import com.example.formulariosjetpackcompose.model.Product
 import com.example.formulariosjetpackcompose.ui.theme.AluveryLazyLayoutTheme
 import java.math.BigDecimal
@@ -48,9 +56,11 @@ fun ProductFormScreen() {
     Column(
         Modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
+            .padding(horizontal = 16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
+        Spacer(Modifier)
         Text(
             fontSize = 28.sp,
             text = "Criando o produto.",
@@ -58,6 +68,17 @@ fun ProductFormScreen() {
         )
         var url by remember {
             mutableStateOf("")
+        }
+        if (url.isNotBlank()) {
+            AsyncImage(
+                model = url, contentDescription = null,
+                Modifier
+                    .fillMaxWidth()
+                    .height(200.dp),
+                contentScale = ContentScale.Crop,
+                placeholder = painterResource(id = R.drawable.placeholder),
+                error = painterResource(id = R.drawable.placeholder)
+            )
         }
         TextField(
             value = url,
@@ -128,6 +149,7 @@ fun ProductFormScreen() {
         }, Modifier.fillMaxWidth()) {
             Text(text = "Salvar")
         }
+        Spacer(Modifier)
     }
 }
 
